7ed6804c9d2bb59e973bcacbcce53ba550889cad
Nikhil Renjith <nikhilrenjith3@gmail.com>
2023-06-15 13:50:19 +0530

    added all the inputs in component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,169 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Axios from "axios";
+import "./App.css";
+import * as weathericons from "weather-icons-react";
 
-function App() {
+const key = "ae0d055343227e09d072824637bc948c";
+
+const App = () => {
+  const [city, setCity] = useState("");
+  const [data, setData] = useState();
+  const [weather, setWeaather] = useState("");
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const geticon =
+    weather === "few clouds" ? (
+      <weathericons.WiCloud />
+    ) : weather === "scattered clouds" ? (
+      <weathericons.WiCloudy />
+    ) : weather === "overcast clouds" ? (
+      <weathericons.WiDaySunnyOvercast />
+    ) : weather === "broken clouds" ? (
+      <weathericons.WiCloudyGusts />
+    ) : weather === "shower rain" ? (
+      <weathericons.WiRainMix />
+    ) : weather === "	rain" ? (
+      <weathericons.WiRain />
+    ) : weather === "thunderstorm" ? (
+      <weathericons.WiThunderstorm />
+    ) : weather === "snow" ? (
+      <weathericons.WiSnow />
+    ) : weather === "mist" ? (
+      <weathericons.WiFog />
+    ) : (
+      <weathericons.WiDaySunny />
+    );
+  const d = new Date();
+  const day = weekday[d.getDay()];
+  const formattedDate = d.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const fetchData = async () => {
+    try {
+      const response = await Axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+      );
+      setData(response.data);
+      setWeaather(response.data.weather[0].description);
+      console.log(response.data);
+    } catch (err) {
+      alert("Please enter a valid city name");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="page-content page-container" id="page-content">
+        <div className="padding">
+          <div className="row container d-flex justify-content-center">
+            <div className="col-lg-8 grid-margin stretch-card">
+              <div className="inputs">
+                <input
+                  type="text"
+                  className="search"
+                  onChange={(e) => setCity(e.target.value)}
+                  placeholder="Enter the city name"
+                ></input>
+
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={fetchData}
+                >
+                  Fetch
+                </button>
+                <div></div>
+              </div>
+              <div className="card card-weather">
+                <div className="card-body">
+                  <div className="weather-date-location">
+                    <h3>{day}</h3>
+                    <p className="text-gray">
+                      <span className="weather-date">{formattedDate}</span>
+                      <span className="weather-location">
+                        {data && (
+                          <div>
+                            {data.name}, {data.sys.country}
+                          </div>
+                        )}
+                      </span>
+                    </p>
+                  </div>
+                  <div className="weather-data d-flex">
+                    <div className="mr-auto">
+                      <h4 className="display-3">
+                        <div className="temp">
+                          {data && (
+                            <div>{(data.main.temp - 273.15).toFixed(1)}</div>
+                          )}
+                          <span className="symbol">&deg;</span>C
+                        </div>
+                      </h4>
+                      <div className="switch">{geticon}</div>
+                      <p>
+                        {data && (
+                          <div>
+                            {data.weather[0].description.replace(
+                              /\b\w/g,
+                              (match) => match.toUpperCase()
+                            )}
+                          </div>
+                        )}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="details">
+                    <div className="values">
+                      {/*Humidity*/}
+                      <weathericons.WiHumidity className="icons" />
+                      <span>{data && <div>{data.main.humidity} %</div>}</span>
+                    </div>
+                    <div className="values">
+                      {/*Pressure*/}
+                      <weathericons.WiBarometer className="icons" />
+                      <span>{data && <div>{data.main.pressure} hPa</div>}</span>
+                    </div>
+                    <div className="values">
+                      {/*Wind Speed*/}
+                      <weathericons.WiCloudyWindy className="icons" />
+                      <span>
+                        {data && (
+                          <div>{(data.wind.speed * 3.6).toFixed(1)} Kmph</div>
+                        )}
+                      </span>
+                    </div>
+                    <div className="values">
+                      {/*Wind Gust*/}
+                      <weathericons.WiCloudyGusts className="icons" />
+                      <span>
+                        {data && (
+                          <div>{(data.wind.gust * 3.6).toFixed(1)} Kmph</div>
+                        )}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="details2">
+                    <div>Humidity</div>
+                    <div>Pressure</div>
+                    <div>Wind Speed</div>
+                    <div>Wind Gust</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
